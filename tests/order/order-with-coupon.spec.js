5e82c0dc5f33a3ac8538b3101bae4f9da9e8117d
Aliaksandr Yeutushkou <Aliaksandr_Yeutushkou@epam.com>
2025-08-26 17:08:34 +0300

    Update order test

--- tests/order/order-with-coupon.spec.js
+++ tests/order/order-with-coupon.spec.js
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-import { CartPage } from '../../core/ui/pages/cart.page.js';
-import { LoginPage } from '../../core/ui/pages/login.page.js';
-import { ProductPage } from '../../core/ui/pages/product.page.js';
-
+import { CouponFactory } from '../../core/models/coupon-factory.js';
 import { UserFactory } from '../../core/models/user-factory.js';
+import { CartPage } from '../../core/screens/pages/cart.page.js';
+import { LoginPage } from '../../core/screens/pages/login.page.js';
+import { ProductPage } from '../../core/screens/pages/product.page.js';
 
 test.describe('Order with coupon flow', () => {
   test('registered user can use coupon for registered users and unlimited usage', async ({ page }) => {
@@ -14,8 +14,7 @@ test.describe('Order with coupon flow', () => {
     const loginPage = new LoginPage(page);
 
     await loginPage.header.goToLogin();
-    await loginPage.login(user.email, user.password);
-    await page.waitForURL(/customer_token/);
+    await loginPage.login(user);
     await loginPage.header.goHome();
 
     const productLink = page.locator('//div[@class="description"]//a[contains(@href, "product_id=40")]');
@@ -29,7 +28,22 @@ test.describe('Order with coupon flow', () => {
     await productPage.addToCart();
     await productPage.header.goToCart();
 
-    const cartPage = new CartPage(page);
+    let cartPage = new CartPage(page);
+
+    const subTotalBeforeCouponUsage = await cartPage.getSubTotalValue();
+    const totalBeforeCouponUsage = await cartPage.getTotalValue();
+
+    await cartPage.expandCouponOptions();
+    await cartPage.useCoupon(CouponFactory.getPercentDiscountUnlimitedCouponForRegisteredUsers());
+
+    const subTotalAfterCouponUsage = await cartPage.getSubTotalValue();
+    const totalAfterCouponUsage = await cartPage.getTotalValue();
+
+    console.log('Total before coupon: ' + totalBeforeCouponUsage);
+    console.log('Total after coupon: ' + totalAfterCouponUsage);
+
+    await expect(subTotalAfterCouponUsage === subTotalBeforeCouponUsage).toBeTruthy();
+    await expect(totalAfterCouponUsage === totalBeforeCouponUsage * 0.85).toBeTruthy();
 
     await cartPage.clearCart();
   });
