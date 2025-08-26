fb8ee655d08edc95dced7078a12398db92c3044f
Aliaksandr Yeutushkou <Aliaksandr_Yeutushkou@epam.com>
2025-08-26 17:05:44 +0300

    Update login method usage

--- tests/login-and-registration/login.spec.js
+++ tests/login-and-registration/login.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-import { LoginPage } from '../../core/ui/pages/login.page.js';
 import { UserFactory } from '../../core/models/user-factory.js';
+import { LoginPage } from '../../core/screens/pages/login.page.js';
 
 test.describe('Login flow', () => {
   test('registered user can log in to shop', async ({ page }) => {
@@ -11,7 +11,7 @@ test.describe('Login flow', () => {
     const loginPage = new LoginPage(page);
 
     await loginPage.header.goToLogin();
-    await loginPage.login(user.email, user.password);
+    await loginPage.login(user);
     await page.waitForURL(/customer_token/);
     await expect(loginPage.rightAccordion.logoutLink).toBeVisible();
   });
